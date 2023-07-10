3272d1d0504a8faea65678c40fb166e00e33db97
AndreaRomano02 <andy.romano2002@gmail.com>
2023-07-10 14:46:21 +0200

    Add library of FontAwesome

--- src/main.js
+++ src/main.js
@@ -2,4 +2,15 @@ import { createApp } from 'vue';
 import './assets/scss/style.scss';
 import App from './App.vue';
 
-createApp(App).mount('#app');
+/* import the fontawesome core */
+import { library } from '@fortawesome/fontawesome-svg-core';
+
+/* import font awesome icon component */
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+
+/* import specific icons */
+import { faChevronLeft } from '@fortawesome/free-solid-svg-icons';
+import { faChevronRight } from '@fortawesome/free-solid-svg-icons';
+
+library.add(faChevronLeft, faChevronRight);
+createApp(App).component('FontAwesomeIcon', FontAwesomeIcon).mount('#app');
