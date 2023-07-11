707ed4cd92464166c3ff1508da4c51a0c9a5e306
AndreaRomano02 <andy.romano2002@gmail.com>
2023-07-11 13:09:17 +0200

    Add Header Section

---
+++ src/components/Main/Section/HeaderSection.vue
@@ -0,0 +1,43 @@
+<script>
+export default {
+  props: {
+    items: Array,
+  }
+}
+</script>
+
+<template>
+  <header>
+    <h3>
+      <slot></slot>
+    </h3>
+    <div class="right-header d-flex gap-3">
+      <div v-for="(item, i) in items" :key="i">
+        {{ item }}
+      </div>
+    </div>
+  </header>
+</template>
+
+<style lang="scss" scoped>
+@use '@/assets/scss/vars' as *;
+
+header {
+  text-transform: uppercase;
+  font-weight: bolder;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+h3 {
+  font-weight: bold;
+}
+
+.right-header>div {
+  background-color: $secondary;
+  color: white;
+  padding: 10px;
+  border-radius: 10px;
+}
+</style>
